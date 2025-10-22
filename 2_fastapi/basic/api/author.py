b124b26c2d9c3f4c62d501d1296b7d9dd6a7f427
tongwei <tongweizj@gmail.com>
2025-10-22 16:03:42 -0400

    update

--- 2_fastapi/basic/api/author.py
+++ 2_fastapi/basic/api/author.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from typing import Annotated
 
+# author 接口
+# 专门用于尝试 fastapi如下功能
+# - Query Parameters
 api_author = APIRouter()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
@@ -7,12 +11,26 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 # async def author_home():
 #     return {"mesage:": "Hi all author!"}
 
+@api_author.get("/")
+# basic version
+# async def read_author(q: str | None = None):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# Annotated + Query version
+async def read_author(q: Annotated[str | None, Query(max_length=10)] = 'Hi'):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 @api_author.get("/list")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
 
-# 默认值
+# Query 默认值、字符串验证
 @api_author.get("/{item_id}")
 async def read_item(item_id: str, q: str | None = None):
     if q:
--- 2_fastapi/basic/api/book.py
+++ 2_fastapi/basic/api/book.py
@@ -1,10 +1,21 @@
 from fastapi import APIRouter
 from enum import Enum
+from pydantic import BaseModel
+# book 接口
+# 专门用于尝试 fastapi如下功能
+# - request body
 
 class BookName(str, Enum):
     bible = "Bible2"
     hp = "HarryPotter"
     jttw = "Journey to the West"
+
+# data model
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 api_book = APIRouter()
 
@@ -17,6 +28,20 @@ async def books_get():
 @api_book.get("/{book_id}")
 async def get_book_id(book_id:int):
     return {"book_id": book_id}
+
+
+@api_book.put("/{book_id}")
+async def update_book(book_id: int, item: Item):
+    return {"book_id": book_id, **item.dict()}
+
+@api_book.put("/q/{book_id}")
+async def update_qbook(book_id: int, item: Item, q: str | None = None):
+    result = {"book_id": book_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
 
 # 路径顺序
 # 相同函数名没有关系，多模态
@@ -37,3 +62,11 @@ async def read_book_name(book_name: BookName):
 
     if book_name.value == "HarryPotter":
         return {"book_name": book_name, "message": "It is a goood book!"}
+
+@api_book.post("/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
