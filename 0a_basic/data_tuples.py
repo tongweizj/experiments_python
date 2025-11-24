e102197787735724a2bdc03731ce2b7f2e2a0e5d
tongwei <tongweizj@gmail.com>
2025-11-23 19:40:26 -0500

    update

---
+++ 0a_basic/data_tuples.py
@@ -0,0 +1,17 @@
+a = (1, 2, 3)
+c = 1,
+b = [1, 2, 3],  # 注意后面的‘，’，这就创建一个tuple，而不是list
+
+# print(a, type(a))
+# print(b, type(b))
+
+
+
+# t1 = a + (1000, 10000)
+# print(t1)
+
+a = a + (1000, 10000)
+# print(a)
+
+print(a.count(1))
+print(a.count(2))
